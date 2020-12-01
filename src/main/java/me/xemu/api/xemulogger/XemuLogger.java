d6edee78a4a7d13579a42e452b1d4f1a30223716
Code By Xemu <74688700+codebyxemu@users.noreply.github.com>
2020-12-01 23:25:59 +0100

    Update XemuLogger.java

--- src/main/java/me/xemu/api/xemulogger/XemuLogger.java
+++ src/main/java/me/xemu/api/xemulogger/XemuLogger.java
@@ -1,17 +1,38 @@
 package me.xemu.api.xemulogger;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class XemuLogger extends JavaPlugin {
+public final class XemuLogger extends JavaPlugin
+{
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+    private static String prefix = "";
 
-    }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
-}
+    public static String setPrefix(String value)
+    {
+        return prefix = "["+value+"] ";
+    };
+
+    public static void log(Object object)
+    {
+        if(prefix == "" || prefix == null)
+        {
+            System.out.println("[XemuLogger] Exception in log()");
+            System.out.println("Please set a prefix using setPrefix(String value)");
+            return;
+        };
+        System.out.println(prefix + object);
+    };
+
+    @Override public void onEnable()
+    {
+        System.out.println(prefix + "Loaded Logger");
+    };
+
+    @Override public void onDisable()
+    {
+        System.out.println(prefix + "Disabled Logger");
+    };
+    
+};
